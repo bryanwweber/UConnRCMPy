182f21ced9742c2cfe7b66ca28f08616f3b1eb10
Bryan W. Weber <bryan@bryanwweber.com>
2015-05-27 12:21:40 -0400

    Add barebones setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UConnRCMPy',
+    version='0.1.0',
+    description='A module to process RCM data',
+    url='https://github.com/bryanwweber/UConnRCMPy',
+    author='Bryan W. Weber',
+    author_email='weber@engr.uconn.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages(),
+)
