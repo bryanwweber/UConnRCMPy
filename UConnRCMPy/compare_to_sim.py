f76dd4221f46a0b893295a44f6226fc2d2ac6ff7
Bryan W. Weber <bryan.w.weber@gmail.com>
2015-05-30 23:31:27 -0400

    Convert compare_to_sim to a function
    So that it can be imported in __init__. Rearrange the module imports

--- UConnRCMPy/compare_to_sim.py
+++ UConnRCMPy/compare_to_sim.py
@@ -4,40 +4,49 @@ Created on Thu May 21 17:55:00 2015
 
 @author: weber
 """
+# System imports
+from glob import glob
+
+# Third-party imports
 import numpy as np
 import matplotlib.pyplot as plt
-from glob import glob
-from pressure_traces import copy, derivative
 import yaml
 
-simdata = np.genfromtxt('export.csv', delimiter=',', skip_header=1)
-simtime = simdata[:, 0]
-simvolume = simdata[:, 1]
-simtemperature = simdata[:, 2]
-simpressure = simdata[:, 3]
+# Local imports
+from .pressure_traces import copy, derivative
 
-flist = glob('*pressure.txt')
-expdata = np.genfromtxt(flist[0])
-exptime = expdata[:, 0]
-exppressure = expdata[:, 1]
 
-fig = plt.figure(2)
-ax = fig.add_subplot(1, 1, 1)
-ax.plot(exptime, exppressure)
-ax.plot(simtime, simpressure)
-m = plt.get_current_fig_manager()
-m.window.showMaximized()
+def compare_to_sim():
+    simdata = np.genfromtxt('export.csv', delimiter=',', skip_header=1)
+    simtime = simdata[:, 0]
+    # simvolume = simdata[:, 1]
+    simtemperature = simdata[:, 2]
+    simpressure = simdata[:, 3]
 
-maxT = np.amax(simtemperature)
-if maxT > 1200:
-    with open('volume-trace.yaml') as yaml_file:
-        y = yaml.load(yaml_file)
+    flist = glob('*pressure.txt')
+    if not len(flist) == 1:
+        flist = [input('Input the experimental pressure trace file name: ')]
+    expdata = np.genfromtxt(flist[0])
+    exptime = expdata[:, 0]
+    exppressure = expdata[:, 1]
 
-    comptime = y['comptime']
-    dpdt = derivative(simtime, simpressure)
-    ign_delay = simtime[np.argmax(dpdt)]*1000 - comptime
-    print('{:.6f}'.format(ign_delay))
-    copy(str(ign_delay))
-else:
-    print('{:.0f}'.format(maxT))
-    copy(str(maxT))
+    fig = plt.figure(2)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(exptime, exppressure)
+    ax.plot(simtime, simpressure)
+    m = plt.get_current_fig_manager()
+    m.window.showMaximized()
+
+    maxT = np.amax(simtemperature)
+    if maxT > 1200:
+        with open('volume-trace.yaml') as yaml_file:
+            y = yaml.load(yaml_file)
+
+        comptime = y['comptime']
+        dpdt = derivative(simtime, simpressure)
+        ign_delay = simtime[np.argmax(dpdt)]*1000 - comptime
+        print('{:.6f}'.format(ign_delay))
+        copy(str(ign_delay))
+    else:
+        print('{:.0f}'.format(maxT))
+        copy(str(maxT))
