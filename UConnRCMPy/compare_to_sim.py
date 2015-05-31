1c1150508dc25cf971dc39736c4d07ce091fe2b8
Bryan W. Weber <bryan.w.weber@gmail.com>
2015-05-30 23:31:42 -0400

    Add comments and docstrings to compare_to_sim

--- UConnRCMPy/compare_to_sim.py
+++ UConnRCMPy/compare_to_sim.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 21 17:55:00 2015
-
-@author: weber
+Compare simulated reactive pressure trace to the corresponding reactive trace
 """
 # System imports
 from glob import glob
@@ -17,12 +15,22 @@ from .pressure_traces import copy, derivative
 
 
 def compare_to_sim():
+    """
+    This function reads the experimental pressure trace from output in
+    the volumetrace function and the output from a CHEMKIN-Pro simulation
+    and plots them to verify that the pressure traces match. It copies either
+    the simulated ignition delay or the EOC temperature to the clipboard,
+    depending on the maximum temperature.
+    """
+    # Load the simulation data file
     simdata = np.genfromtxt('export.csv', delimiter=',', skip_header=1)
     simtime = simdata[:, 0]
     # simvolume = simdata[:, 1]
     simtemperature = simdata[:, 2]
     simpressure = simdata[:, 3]
 
+    # Load the experimental pressure trace. Try the glob function first
+    # and if it fails, ask the user for help.
     flist = glob('*pressure.txt')
     if not len(flist) == 1:
         flist = [input('Input the experimental pressure trace file name: ')]
@@ -30,6 +38,7 @@ def compare_to_sim():
     exptime = expdata[:, 0]
     exppressure = expdata[:, 1]
 
+    # Plot the pressure traces together
     fig = plt.figure(2)
     ax = fig.add_subplot(1, 1, 1)
     ax.plot(exptime, exppressure)
@@ -37,6 +46,9 @@ def compare_to_sim():
     m = plt.get_current_fig_manager()
     m.window.showMaximized()
 
+    # Compute the maximum temperature; if ignition has occurred, judged
+    # by maxT > 1200 K, compute the ignition delay. Otherwise, the maxT
+    # is Tc. In either case, copy the value to the clipboard.
     maxT = np.amax(simtemperature)
     if maxT > 1200:
         with open('volume-trace.yaml') as yaml_file:
