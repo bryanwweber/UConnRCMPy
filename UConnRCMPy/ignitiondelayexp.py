1d82a6f4559fa2ff18b403884a4d36198b52fa34
Bryan W. Weber <bryan@bryanwweber.com>
2015-06-19 16:17:05 -0400

    Convert ignitiondelayexp function to class
    Convert the function computing the ignition delay of the experiments
    from a function to a class.

--- UConnRCMPy/ignitiondelayexp.py
+++ UConnRCMPy/ignitiondelayexp.py
@@ -9,68 +9,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Local imports
-from .pressure_traces import (copy, compress, derivative, file_loader,
-                              ParsedFilename, pressure_to_temperature,
-                              smoothing)
+from .pressure_traces import ReactivePressureTrace
+from .temperature_traces import TemperatureFromPressure
+from .utilities import copy
 
 
-def ignitiondelayexp():
-    """
-    Calculate the ignition delay from an experimental pressure trace
+class ExperimentalIgnitionDelay(ReactivePressureTrace):
+    """Class storing one experimental ignition delay case."""
 
-    This function calculates the ignition delay, given an experimental
-    pressure trace from an RCM experiment.
-    """
-    filename = input('Filename: ')
-    data = file_loader(filename)
-    file_info = ParsedFilename(filename)
-    pres = data[:, 1]*file_info.factor + file_info.pin*1.01325/760
-    time = data[:, 0]
-    smpr = smoothing(pres)
-    pc, pci = compress(smpr)
-    ztim = time - time[pci]
-    freq = np.rint(1/time[1])
-    dpdt = derivative(time, smpr)
-    smdp = smoothing(dpdt, 5)
+    def __init__(self):
+        super().__init__()
+        # offset_points is an offset from the EOC to ensure that if
+        # ignition is weak, the peak in dP/dt from the compression
+        # stroke is not treated as the ignition event. Define points
+        # to start looking for and stop looking for ignition.
+        offset_points = 0.005*self.frequency
+        start_point = self.p_EOC_idx + offset_points
+        end_point = self.p_EOC_idx + offset_points + 100000
 
-    # offt is an offset from the EOC to ensure that if ignition is
-    # weak, the peak in dP/dt from the compression stroke is not
-    # treated as the ignition event.
-    offt = 0.001
+        # The index of the maximum of the smoothed derivative trace
+        # is taken as the point of ignition
+        self.idx_of_ig = np.argmax(self.smdp[start_point:end_point])
 
-    # The index of the maximum of the smoothed derivative trace is taken
-    # as the point of ignition
-    iofig = np.argmax(smdp[(pci + offt*freq):(pci + offt*freq + 200000)])
+        # Take the offset into account when calculating the ignition
+        # delay. The index of ignition is already relative to zero,
+        # so we don't need to subtract any times
+        self.ignition_delay = self.time[self.idx_of_ig + offset_points]*1000
+        self.first_stage = 0.0
 
-    # Computing first stage ignition is disabled at the moment
-    firststage = 0.0
+        pres_to_temp_start_idx = self.p_EOC_idx - 0.03*self.frequency
+        tempp = self.pressure[(pres_to_temp_start_idx):(self.p_EOC_idx)]
+        temperature_trace = TemperatureFromPressure(tempp, self.Tin)
+        self.T_EOC = np.amax(temperature_trace.temperature)
 
-    # Take the offset into account when calculating the time of ignition
-    timeofig = time[iofig + freq*offt]*1000
-    tempp = pres[(pci - 0.03*freq):(pci)]
-    temperature = pressure_to_temperature(tempp, file_info.Tin)
-    T_EOC = max(temperature)
+        # Copy the relevant information to the clipboard for pasting
+        # into a spreadsheet
+        copy('\t'.join(map(str, [
+            self.time_of_day, self.pin, self.Tin, self.p_EOC,
+            self.ignition_delay, self.first_stage, self.T_EOC, self.spacers,
+            self.shims])))
 
-    # Copy the relevant information to the clipboard for pasting into a
-    # spreadsheet
-    copy('\t'.join(map(str, [
-        file_info.timeofday, file_info.pin, file_info.Tin, pc, timeofig,
-        firststage, T_EOC, file_info.spacers, file_info.shims
-        ])))
+        # Plot the smoothed pressure and overlay future runs
+        fig1 = plt.figure(1)
+        ax1 = fig1.add_subplot(1, 1, 1)
+        ax1.plot(self.ztim, self.pressure, label=self.date)
+        m = plt.get_current_fig_manager()
+        m.window.showMaximized()
 
-    # Plot the smoothed pressure and overlay future runs
-    fig1 = plt.figure(1)
-    ax1 = fig1.add_subplot(111)
-    ax1.plot(ztim, smpr, label=file_info.date)
-    m = plt.get_current_fig_manager()
-    m.window.showMaximized()
-
-    # Plot the raw and smoothed pressure and the smoothed derivative
-    # on a new figure every time
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111)
-    ax2.plot(ztim, smpr)
-    ax2.plot(ztim, smdp/1000)
-    ax2.plot(ztim, pres)
-    m = plt.get_current_fig_manager()
-    m.window.showMaximized()
+        # Plot the raw and smoothed pressure and the smoothed derivative
+        # on a new figure every time
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(1, 1, 1)
+        ax2.plot(self.ztim, self.pressure)
+        ax2.plot(self.ztim, self.smdp/1000)
+        ax2.plot(self.ztim, self.pres)
+        m = plt.get_current_fig_manager()
+        m.window.showMaximized()
