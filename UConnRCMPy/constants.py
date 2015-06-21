af217d41a8dea729e1208497e2c8d676680a4dbd
Bryan W. Weber <bryan.w.weber@gmail.com>
2015-06-21 11:53:41 -0400

    Add pdoc docstrings to constants.py

--- UConnRCMPy/constants.py
+++ UConnRCMPy/constants.py
@@ -1,11 +1,19 @@
-import cantera as ct
+"""
+Constants used in UConnRCMPy.
+"""
+import cantera
 
 one_atm_in_torr = 760.0
+"""Conversion between atmosphere and Torr."""
 one_atm_in_bar = 1.01325
+"""Conversion between atmosphere and bar."""
 one_bar_in_pa = 1E5
+"""Conversion between bar and Pascal."""
+cantera_version = None
+"""Installed version of Cantera software."""
 
 try:
-    cantera_version = map(int, ct.__version__.split('.'))
+    cantera_version = map(int, cantera.__version__.split('.'))
 except ValueError:
-    cantera_version = map(int, ct.__version__.split('.')[0:2])
-    cantera_version.append(int(ct.__version__.split('.')[2][0]))
+    cantera_version = map(int, cantera.__version__.split('.')[0:2])
+    cantera_version.append(int(cantera.__version__.split('.')[2][0]))
