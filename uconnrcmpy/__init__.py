11529d7ad4d428bdd9f5a58adc1085a665d4f222
Bryan W. Weber <bryan@bryanwweber.com>
2015-06-27 12:36:11 -0400

    Load the external interface on package import
    Set __init__.py so that the useful classes are loaded when the package
    is loaded

---
+++ uconnrcmpy/__init__.py
@@ -0,0 +1,11 @@
+from .ignitiondelayexp import ExperimentalIgnitionDelay
+from .compare_to_sim import compare_to_sim
+from .volume_trace import VolumeTraceBuilder
+from .nonreactive import NonReactiveExperiments
+
+__all__ = [
+    'ExperimentalIgnitionDelay',
+    'compare_to_sim',
+    'VolumeTraceBuilder',
+    'NonReactiveExperiments',
+]
