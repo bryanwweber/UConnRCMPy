44cd75b45f5607818260f8c3d6ddcaf491672002
Bryan W. Weber <bryan.w.weber@gmail.com>
2015-06-28 01:10:29 -0400

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='UConnRCMPy',
-    version='0.1.0',
+    version='1.0.0',
     description='A package to process RCM data',
     url='https://github.com/bryanwweber/UConnRCMPy',
     author='Bryan W. Weber',
     author_email='weber@engr.uconn.edu',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
