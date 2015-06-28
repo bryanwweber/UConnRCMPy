9da380e55c4f014b53222c73fe2f709556baaa97
Bryan W. Weber <bryan.w.weber@gmail.com>
2015-06-28 09:31:18 -0400

    Rename console script

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'process-ignition-loop=uconnrcmpy.ign_loop:main',
+            'ignloop=uconnrcmpy.ign_loop:main',
         ],
     },
 )
