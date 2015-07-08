4742f587e3e66fd1916dcb7200517e2ac06ddcf4
Bryan W. Weber <bryan@bryanwweber.com>
2015-07-08 09:55:15 -0400

    Enforce Python >= 3.4 on import of the package
    Python 3.4 is required for the pathlib module

--- uconnrcmpy/__init__.py
+++ uconnrcmpy/__init__.py
@@ -1,3 +1,7 @@
+import sys
+if sys.version_info[0] < 3 and sys.version_info[1] < 4:
+    raise Exception('Python 3.4 is required to use this package.')
+
 from .ignitiondelayexp import ExperimentalIgnitionDelay
 from .compare_to_sim import CompareToSimulation
 from .volume_trace import VolumeTraceBuilder
