64fa15a7fe7b5dd615527cd00d2629bc22e8052b
Bryan W. Weber <bryan@bryanwweber.com>
2016-01-09 09:57:03 -0500

    Remove non-existing imports in __init__

--- uconnrcmpy/__init__.py
+++ uconnrcmpy/__init__.py
@@ -1,15 +1,3 @@
 import sys
 if sys.version_info[0] < 3 and sys.version_info[1] < 4:
-    raise Exception('Python 3.4 is required to use this package.')
-
-from .ignitiondelayexp import ExperimentalIgnitionDelay
-from .compare_to_sim import CompareToSimulation
-from .volume_trace import VolumeTraceBuilder
-from .nonreactive import NonReactiveExperiments
-
-__all__ = [
-    'ExperimentalIgnitionDelay',
-    'CompareToSimulation',
-    'VolumeTraceBuilder',
-    'NonReactiveExperiments',
-]
+    raise Exception('Python 3.4 or greater is required to use this package.')
