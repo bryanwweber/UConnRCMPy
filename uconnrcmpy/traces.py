03814a29d3d0f34b30d2bf5a2fa5868ae77677e9
Bryan W. Weber <bryan@bryanwweber.com>
2016-01-08 19:50:52 -0500

    Move VolumeTraceFromPressure to traces module
    Remove unnecessary volume_traces module.

--- uconnrcmpy/traces.py
+++ uconnrcmpy/traces.py
@@ -197,3 +197,18 @@ class PressureFromVolume(object):
             gas.SV = initial_entropy, v*initial_volume
             self.pressure[i] = gas.P/one_bar_in_pa
 
+
+class VolumeFromPressure(object):
+
+    def __init__(self, pressure, v_in, T_in):
+        gas = ct.Solution('species.cti')
+        if cantera_version[1] > 2:
+            gas.DP = 1.0/v_in, pressure[0]*one_bar_in_pa
+        else:
+            gas.TP = T_in, pressure[0]*one_bar_in_pa
+        initial_entropy = gas.entropy_mass
+        initial_density = gas.density
+        self.volume = np.zeros((len(pressure)))
+        for i, p in enumerate(pressure):
+            gas.SP = initial_entropy, p*one_bar_in_pa
+            self.volume[i] = v_in*initial_density/gas.density
