7db628ecfb3736ab1dc246bbbaa30c7f8d42c0c1
Bryan W. Weber <bryan@bryanwweber.com>
2016-01-23 17:04:16 -0500

    Refactor ign_loop to work with new classes

--- uconnrcmpy/ign_loop.py
+++ uconnrcmpy/ign_loop.py
@@ -5,16 +5,8 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 # Local imports
-from .ignitiondelayexp import ExperimentalIgnitionDelay
+from .dataprocessing import Experiment
 from .utilities import copy
-
-
-class LoopIgnitionDelay(ExperimentalIgnitionDelay):
-
-    def __init__(self, filename):
-        super(ExperimentalIgnitionDelay, self).__init__(filename)
-        self.calculate_ignition_delay()
-        self.calculate_EOC_temperature()
 
 
 def main(path='.', plot=False):
@@ -27,13 +19,14 @@ def main(path='.', plot=False):
 
     for f in p.glob('[0-3]*.txt'):
         print(f)
-        case = LoopIgnitionDelay(f.resolve())
+        case = Experiment(f.resolve())
         result.append('\t'.join(map(str, [
-            case.time_of_day, case.pin, case.Tin, case.p_EOC,
-            case.ignition_delay, case.first_stage, case.T_EOC,
-            case.spacers, case.shims])))
+            case.experiment_parameters['time_of_day'], case.experiment_parameters['pin'],
+            case.experiment_parameters['Tin'], case.pressure_trace.p_EOC, case.ignition_delay,
+            case.first_stage, case.T_EOC, case.experiment_parameters['spacers'],
+            case.experiment_parameters['shims']])))
         if plot:
-            ax.plot(case.ztim, case.pressure, label=case.date)
+            ax.plot(case.zeroed_time, case.pressure, label=case.experiment_parameters['date'])
 
     copy('\n'.join(sorted(result)))
     print('Finished')
