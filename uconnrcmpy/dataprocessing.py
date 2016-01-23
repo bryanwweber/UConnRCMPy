32d3b90ae98076d692e4b91f929f8c38c9a29999
Bryan W. Weber <bryan@bryanwweber.com>
2016-01-23 17:07:46 -0500

    Move ign_loop.main to dataprocessing.process_in_loop
    Fix setup.py to create a shell script for the new code

--- uconnrcmpy/dataprocessing.py
+++ uconnrcmpy/dataprocessing.py
@@ -565,3 +565,29 @@ class Experiment(object):
         ax2.plot(self.pressure_trace.zeroed_time, self.pressure_trace.smoothed_derivative/1000)
         m = plt.get_current_fig_manager()
         m.window.showMaximized()
+
+
+def process_in_loop(path='.', plot=False):
+    p = Path(path)
+    result = []
+
+    if plot:
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+
+    for f in p.glob('[0-3]*.txt'):
+        print(f)
+        case = Experiment(f.resolve())
+        result.append('\t'.join(map(str, [
+            case.experiment_parameters['time_of_day'], case.experiment_parameters['pin'],
+            case.experiment_parameters['Tin'], case.pressure_trace.p_EOC, case.ignition_delay,
+            case.first_stage, case.T_EOC, case.experiment_parameters['spacers'],
+            case.experiment_parameters['shims']])))
+        if plot:
+            ax.plot(case.zeroed_time, case.pressure, label=case.experiment_parameters['date'])
+
+    copy('\n'.join(sorted(result)))
+    print('Finished')
+
+if __name__ == '__main__':
+    process_in_loop()
