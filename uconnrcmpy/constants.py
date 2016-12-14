1525d00be1f1bfffdeaf23e2a6811ffd229e0103
Bryan W. Weber <bryan.w.weber@gmail.com>
2016-12-14 12:35:07 -0500

    Add types to constants in constants.py

--- uconnrcmpy/constants.py
+++ uconnrcmpy/constants.py
@@ -4,13 +4,13 @@ Constants used in UConnRCMPy.
 import cantera
 
 one_atm_in_torr = 760.0
-"""Conversion between atmosphere and Torr."""
+"""`float`: Conversion between atmosphere and Torr."""
 one_atm_in_bar = 1.01325
-"""Conversion between atmosphere and bar."""
+"""`float`: Conversion between atmosphere and bar."""
 one_bar_in_pa = 1E5
-"""Conversion between bar and Pascal."""
+"""`float`: Conversion between bar and Pascal."""
 cantera_version = None
-"""Installed version of Cantera software."""
+"""`list`: Installed version of Cantera software."""
 
 try:
     cantera_version = list(map(int, cantera.__version__.split('.')))
