3097748c9783f22e23c90267ca3504d8b058a03a
Bryan W. Weber <bryan.w.weber@gmail.com>
2016-12-22 15:10:32 -0500

    Split dataprocessing into conditions and experiments modules

--- uconnrcmpy/__init__.py
+++ uconnrcmpy/__init__.py
@@ -1,5 +1,5 @@
 import sys
-from .dataprocessing import Condition, AltCondition, process_folder, process_alt_folder
+from .conditions import Condition, AltCondition, process_folder, process_alt_folder
 
 if sys.version_info[0] < 3 and sys.version_info[1] < 4:
     raise Exception('Python 3.4 or greater is required to use this package.')
