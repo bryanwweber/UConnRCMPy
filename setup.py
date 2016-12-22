29f0703d9c37a50ca28223b47ac32c70da9c2cff
Bryan W. Weber <bryan.w.weber@gmail.com>
2016-12-22 15:47:10 -0500

    Move version information to _version.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup, find_packages
+import os.path as op
+
+with open(op.join(op.dirname(op.realpath(__file__)), 'uconnrcmpy', '_version.py')) as version_file:
+    exec(version_file.read())
+
+with open(op.join(op.dirname(op.realpath(__file__)), 'README.md')) as readme_file:
+    readme = readme_file.read()
+
+with open(op.join(op.dirname(op.realpath(__file__)), 'CHANGELOG.md')) as changelog_file:
+    changelog = changelog_file.read()
 
 setup(
     name='UConnRCMPy',
-    version='2.1.0',
+    version=__version__,
     description='A package to process RCM data',
+    long_description=readme + '\n\n' + changelog,
     url='https://github.com/bryanwweber/UConnRCMPy',
     author='Bryan W. Weber',
     author_email='bryan.weber@uconn.edu',
@@ -14,6 +25,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(),
     entry_points={
--- uconnrcmpy/__init__.py
+++ uconnrcmpy/__init__.py
@@ -1,5 +1,6 @@
 import sys
 from .conditions import Condition, AltCondition, process_folder, process_alt_folder
+from ._version import __version__
 
 if sys.version_info[0] < 3 and sys.version_info[1] < 4:
     raise Exception('Python 3.4 or greater is required to use this package.')
