f47601c0c723837dd9c3c1efdc2957fbd20c56b0
Bryan W. Weber <bryan.w.weber@gmail.com>
2017-02-09 12:50:24 -0500

    Add tests for Experiment

---
+++ uconnrcmpy/tests/test_experiments.py
@@ -0,0 +1,32 @@
+"""
+Test module for the experiments module
+"""
+import numpy as np
+import os
+import pytest
+from ..experiments import Experiment
+
+
+@pytest.fixture(scope='module')
+def files():
+    datadir = os.path.dirname(__file__)
+    cti_file = os.path.join(datadir, 'species.cti')
+    reacfile = os.path.join(datadir, '00_in_00_mm_333K-1146t-100x-21-Jul-15-1226.txt')
+    nonrfile = os.path.join(datadir, 'NR_00_in_00_mm_333K-1137t-100x-21-Jul-15-1251.txt')
+    return {'cti_file': cti_file, 'reacfile': reacfile, 'nonrfile': nonrfile}
+
+
+def test_create_reactive_experiment(files):
+    exp = Experiment(files['reacfile'],
+                     cti_file=files['cti_file'], copy=False)
+    assert np.isclose(exp.ignition_delay, 66.22)
+    assert np.isclose(exp.T_EOC, 765.374)
+    assert np.isclose(exp.first_stage, 64.04)
+
+
+def test_create_nonreactive_experiment(files):
+    exp = Experiment(files['nonrfile'],
+                     cti_file=files['cti_file'], copy=False)
+    assert np.isclose(exp.ignition_delay, 0.0)
+    assert np.isclose(exp.T_EOC, 0.0)
+    assert np.isclose(exp.first_stage, 0.0)
