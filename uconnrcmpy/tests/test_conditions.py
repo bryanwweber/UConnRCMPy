72dcd06e5270fc133f9430544f6306d30e0b87f6
Bryan W. Weber <bryan.w.weber@gmail.com>
2017-02-09 12:50:13 -0500

    Use copy=False on Condition tests so it works on Travis

--- uconnrcmpy/tests/test_conditions.py
+++ uconnrcmpy/tests/test_conditions.py
@@ -19,13 +19,11 @@ class TestCondition(object):
         cti_file = os.path.join(os.path.dirname(__file__), 'species.cti')
         Condition(cti_file=cti_file, plotting=False)
 
-    @pytest.mark.skipif(os.getenv('TRAVIS_OS_NAME') == 'linux',
-                        reason="Travis-linux is headless, so xclip won't work")
     def test_add_experiment(self):
         datadir = os.path.dirname(__file__)
         cti_file = os.path.join(datadir, 'species.cti')
         reacfile = os.path.join(datadir, '00_in_00_mm_333K-1146t-100x-21-Jul-15-1226.txt')
         nonrfile = os.path.join(datadir, 'NR_00_in_00_mm_333K-1137t-100x-21-Jul-15-1251.txt')
         c = Condition(cti_file=cti_file, plotting=False)
-        c.add_experiment(reacfile)
-        c.add_experiment(nonrfile)
+        c.add_experiment(reacfile, copy=False)
+        c.add_experiment(nonrfile, copy=False)
