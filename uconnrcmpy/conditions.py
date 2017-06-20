185e188e834aa3124c6ee3d6d5e10659ddb479f7
ruozhou <ruozhou.fang@uconn.edu>
2017-06-20 16:46:59 -0400

    Fix 'change_EOC_time' malfunction when reactive_experiments recognized as non-reactive_experiments
    (1)Add the misrecongnized "non-reactive experiment" back to
    reactive_experiment list. Also creat a reactive-case plot instead of a
    nonreactive-case plot for further modification.
    (2)Add the error message when EOC_index out of range.

--- uconnrcmpy/conditions.py
+++ uconnrcmpy/conditions.py
@@ -347,7 +347,11 @@ class Condition(object):
             if str(experiment) in self.reactive_experiments:
                 experiment = self.reactive_experiments[str(experiment)]
             elif str(experiment) in self.nonreactive_experiments:
-                experiment = self.nonreactive_experiments[str(experiment)]
+                exp = Experiment(experiment, cti_file=self.cti_file)
+                self.reactive_experiments[exp.file_path.name] = exp
+                if self.plotting:
+                    self.plot_reactive_figures(exp)
+                experiment = self.reactive_experiments[str(experiment)]
             else:
                 raise ValueError('{} could not be found in the Condition. '
                                  'Did you add it with add_experiment?'.format(str(experiment)))
