bee947d4a7af205c5b46970353f2e413a90ee963
Bryan W. Weber <bryan.w.weber@gmail.com>
2017-07-25 11:47:03 -0400

    Don't cover the test for Python version

--- uconnrcmpy/__init__.py
+++ uconnrcmpy/__init__.py
@@ -2,5 +2,5 @@ import sys
 from .conditions import Condition, AltCondition, process_folder, process_alt_folder
 from ._version import __version__
 
-if sys.version_info[0] < 3 and sys.version_info[1] < 4:
+if sys.version_info[0] < 3 and sys.version_info[1] < 4:  # pragma: no cover
     raise Exception('Python 3.4 or greater is required to use this package.')
